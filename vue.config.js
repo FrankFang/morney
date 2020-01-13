b91508482661bd8c7f6006e63bf8ffeaf38ab002
frankfang <frankfang1990@gmail.com>
2020-01-13 16:03:32 +0800

    使用 svg-sprite-loader-mod 引入 svg

--- vue.config.js
+++ vue.config.js
@@ -3,28 +3,21 @@ module.exports = {
   lintOnSave: false,
 
   chainWebpack: config => {
+    const dir = path.resolve('src/assets/icons')
+
     config.module
       .rule('svg-sprite')
-      .use('svgo-loader')
-      .loader('svgo-loader')
-      .tap(options => {
-        return {
-          ...options,
-          plugins: [
-            {removeAttrs: {attrs: 'fill'}},
-          ]
-        }
-      })
+      .test(/\.(svg)(\?.*)?$/)
+      .include.add(dir).end()
+      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
+      .end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
+    config.module.rule('svg').exclude.add(dir)
     // https://github.com/vuejs/vue-cli/issues/2398#issuecomment-503582811
     // config.resolve.alias.set('assets',
     //   path.resolve('src/assets'))
   },
-  pluginOptions: {
-    svgSprite: {
-      dir: 'src/assets/icons',
-      test: /\.(svg)(\?.*)?$/,
-      loaderOptions: {extract: false},
-      pluginOptions: {plainSprite: true}
-    }
-  }
+  pluginOptions: {}
 }
